624c312920702c1cedc65ed10734ffd7caa590ae
serhiichayck <chayck7@gmail.com>
2021-05-05 17:31:18 +0300

    Auth and verification email

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')
+    ->middleware('verified')
+    ->name('home');
